3eb9902e08c30ebe1095a9fef61946ccb8780bf7
Kevin Faria <kevintxd73@gmail.com>
2024-08-08 22:28:39 -0300

    ads added

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ WORKDIR /app
 
 RUN yarn install --frozen-lockfile
     
+ENV NEXT_PUBLIC_NEWS_URL=https://api.novidadescripto.com.br/news
+ENV NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID=ca-pub-3646211809176894
+
 RUN yarn run build
     
 EXPOSE 3000:3000
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Ubuntu({ subsets: ["latin"], weight: ["300"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
-        <script async src="https://static.coinstats.app/widgets/v5/cs-widget.js"></script>
+        <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`} 
+        crossOrigin="anonymous"></script>
+        <Script async src="https://static.coinstats.app/widgets/v5/cs-widget.js"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
